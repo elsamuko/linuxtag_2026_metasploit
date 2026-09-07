7dfb8c1cdf61d9488d3037f96ddcb1f9af143d2b
Samuel Albrecht <elsamuko@gmail.com>
2026-09-07 17:23:44 +0200

    chg: print before/after of db

--- access_control/run.sh
+++ access_control/run.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 # exit on error
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export PATH="/opt/metasploit-framework/bin":"/opt/metasploit-framework/embedded/bin":$PATH
+
+function separator {
+    printf "\n\e[1;33m%-$(tput cols)s\e[0m\n" "--$*" | tr " " "-"
+}
+
+separator "dump db"
+(cd "$SCRIPT_DIR/../sql" && ./init_db.py)
+sqlite3 "$SCRIPT_DIR/../sql/database.db" .dump
 
 # check syntax
 ruby -c ./modules/auxiliary/workshop/broken_access_control.rb
@@ -20,14 +27,17 @@ run
 exit
 EOF
 
+separator "run attack"
 echo n | msfconsole \
     --no-database \
     --logger Stdout \
     --quiet \
     --resource run.r | tee result.txt
 
+separator "check result"
 if grep "STATUS: SUCCESS" < result.txt; then
     echo "Updated user to admin rights"
+    sqlite3 "$SCRIPT_DIR/../sql/database.db" .dump
 else
     echo "Failed to update user"
 fi
