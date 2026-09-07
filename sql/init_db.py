66abd7e4c655553622a8f18baeb17d15e7f46a12
Samuel Albrecht <elsamuko@gmail.com>
2026-09-07 17:23:17 +0200

    add: is_admin parameter

--- sql/init_db.py
+++ sql/init_db.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import sqlite3
+from pathlib import Path
 
+Path('database.db').unlink(missing_ok=True)
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
 
@@ -10,13 +12,14 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY,
         username TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL
+        password TEXT NOT NULL,
+        is_admin BOOLEAN NOT NULL DEFAULT FALSE
     )
 ''')
 
 # Insert a dummy user
-cursor.execute("INSERT OR REPLACE INTO users (username, password) VALUES ('admin', 'SuperSecretPassword123')")
-cursor.execute("INSERT OR REPLACE INTO users (username, password) VALUES ('user', 'notadmin')")
+cursor.execute("INSERT OR REPLACE INTO users (username, password, is_admin) VALUES ('admin', 'SuperSecretPassword123', 1)")
+cursor.execute("INSERT OR REPLACE INTO users (username, password, is_admin) VALUES ('user', 'notadmin', 0)")
 
 conn.commit()
 conn.close()
