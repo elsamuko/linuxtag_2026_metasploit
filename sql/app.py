446eba916dcf1985d161cadd3a3e1c9c5030e829
Samuel <elsamuko@gmail.com>
2026-09-12 23:06:29 +0200

    add: comment

--- sql/app.py
+++ sql/app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# simple app with authentication
+# https://dev.mysql.com/doc/connector-python/en/
 
 import sqlite3
 from flask import Flask, request, render_template_string, session
